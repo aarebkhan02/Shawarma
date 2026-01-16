1048a99ce07cb52791b14cb0a11f6bb1614d94fd
aarebkhan02 <aarebkhan123@gmail.com>
2026-01-16 12:21:00 +0530

    Push

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ from bot import model
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
+        "https://shawarmasenpai.vercel.app",  # frontend
+        "http://localhost:5173"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.post("/chat")
 async def chat(request: Request):
